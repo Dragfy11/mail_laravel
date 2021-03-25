d4b5fed31578b9bd3e8e7e89af26a3b09bf7c5bf
Dragfy11 <dragfy_dark@hotmail.com>
2021-03-25 15:54:31 +0100

    mise en place de la newsletter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,14 +17,29 @@
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="{{route('accueil')}}">Accueil</a>
+      </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('contact')}}">Contact</a>
+        </li> 
+      <li class="nav-item">
+        <form action="{{route('newsletter.store')}}" method="POST" class=" pt-1 ml-4">     
+            @csrf  
+            <div class="form-group row bg-secondary p-1 rounded text-light">
+              <label for="email" class="col-sm-3 col-form-label">Newsletter</label>
+              <div class="col">
+                <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+              </div>
+              <button type="submit" class="btn btn-link b-col-2 text-light">Envoyez</button>
+            </div>
+          </form>
       </li>     
-
     </ul>
+    
     <ul class="navbar-nav ">
         @if (Route::has('login'))         
                     @auth
                      <li class="nav-item">
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a class="nav-link"href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                     </li> 
                     <form action="/logout" method="post" class="ml-3 ">
                         @csrf
@@ -32,7 +47,7 @@
                     </form>
                     @else
                      <li class="nav-item">
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                        <a class="nav-link"href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
                     </li> 
                         @if (Route::has('register'))
                         <li class="nav-item">
